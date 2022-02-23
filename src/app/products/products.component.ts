5b2ede7d4eb63ba837f3a170f68ef49b9389e902
Ng20170201 <nikolag857@gmail.com>
2022-02-23 09:47:48 +0100

    drugi deo

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,6 +1,7 @@
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { map, Observable } from 'rxjs';
 
-import { Product} from 'src/models/product.model';
+import { Product } from 'src/models/product.model';
 import { Shop } from 'src/models/shop.model';
 import { ProductService } from './product.service';
 
@@ -10,29 +11,28 @@ import { ProductService } from './product.service';
   styleUrls: ['./products.component.css']
 })
 export class ProductsComponent implements OnInit {
-  shops:Shop[];
-  products:Product[];
-  shopItem:number;
+  shops: Shop[];
+  products: Product[];
+  shopItem: number;
 
-  constructor(public productService:ProductService) { }
+  constructor(public productService: ProductService) { }
 
   ngOnInit(): void {
 
-    
-    this.productService.getAllProduct().subscribe(d=>{
-      this.shops=d;
-      
+
+    this.productService.getAllProduct().subscribe(d => {
+      this.shops = d;
+
     })
-    this.productService.get2Shops().subscribe(d=>{
-     
+    this.productService.get2Shops().subscribe(d => {
       console.log(d);
     });
 
   }
-  selectShop($event:any):void{
-    this.products=[];
-    this.products=this.shops.find(d=>d.Id==this.shopItem).products;
-
-
+  selectShop($event: any): void {
+    this.products = [];
+    this.products = this.shops.find(d => d.Id == this.shopItem).products;
   }
+
+ 
 }
